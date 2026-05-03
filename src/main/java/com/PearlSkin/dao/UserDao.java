d9a8bdc28dd979723f23fe85816398b25a9fb596
AmanGrg05 <amangrg039@gmail.com>
2026-05-03 07:53:26 +0545

    Added abstract methods

--- src/main/java/com/PearlSkin/dao/UserDao.java
+++ src/main/java/com/PearlSkin/dao/UserDao.java
@@ -1,4 +1,10 @@
 package com.PearlSkin.dao;
 
+import com.PearlSkin.entity.User;
+
 public interface UserDao {
+    boolean addUser(User user);
+    User findByUsername(String username);
+    boolean updateUser(User user);
+    boolean deleteUser(int id);
 }
