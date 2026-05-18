46c688b4c44464056f6289ff4f3d3906acb33990
Sumina33 <hasumina189@gmail.com>
2026-05-18 19:23:26 +0545

    website

--- src/main/java/com/PearlSkin/controller/LoginServlet.java
+++ src/main/java/com/PearlSkin/controller/LoginServlet.java
@@ -1,4 +1,56 @@
 package com.PearlSkin.controller;
 
-public class LoginServlet {
+import com.PearlSkin.dao.UserDao;
+import com.PearlSkin.dao.UserDaoImpl;
+import com.PearlSkin.entity.User;
+import com.PearlSkin.utils.CookieUtil;
+import com.PearlSkin.utils.PasswordUtil;
+import com.PearlSkin.utils.SessionUtil;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private final UserDao userDao = new UserDaoImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = userDao.findByUsername(username);
+
+        if (user == null) {
+            request.setAttribute("error", "Invalid username or password");
+            request.getRequestDispatcher("/WEB-INF/views/login.jsp")
+                    .forward(request, response);
+            return;
+        }
+
+        if (!PasswordUtil.checkPassword(password, user.getPassword())) {
+            request.setAttribute("error", "Invalid username or password");
+            request.getRequestDispatcher("/WEB-INF/views/login.jsp")
+                    .forward(request, response);
+            return;
+        }
+
+        SessionUtil.setAttribute(request, "user", user);
+        CookieUtil.addCookie(response, "username", user.getUsername(), 24*60*60);
+        response.sendRedirect(request.getContextPath() + "/dashboard");
+    }
 }
--- src/main/java/com/PearlSkin/dao/UserDaoImpl.java
+++ src/main/java/com/PearlSkin/dao/UserDaoImpl.java
@@ -1,4 +1,41 @@
 package com.PearlSkin.dao;
 
-public class UserDaoImpl {
+import com.PearlSkin.entity.User;
+import com.PearlSkin.utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public boolean addUser(User user) {
+        if (findByUsername(user.getUsername()) != null) {
+            System.out.println("Username already exists: " + user.getUsername());
+            return false;
+        }
+        if (findByEmail(user.getEmail()) != null) {
+            System.out.println("Email already exits: " + user.getEmail());
+            return false;
+        }
+
+        Connection con = null;
+        try{
+            conn = DatabaseConnection.getConnection();
+            String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error adding user: " + e.getMessage());
+            return false;
+        }finally{
+            DatabaseConnection.closeConnection(conn);
+        }
+    }
 }
